7111543b873b6e64669e264b70672d89f614dc5e
Hari Om Singh <www.hariom03.com@gmail.com>
2021-08-14 23:48:09 +0530

    add 404 page

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-"use strict";
 const express = require("express");
 const app = express();
 const path = require("path");
@@ -6,7 +5,6 @@ const bodyParser = require("body-parser");
 const fetch = require("node-fetch");
 app.set("view engin", "ejs");
 
-// app.use(express.static(path.join("public")));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
@@ -20,11 +18,13 @@ app.post("/weather", (req, res) => {
       `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=30c06c817dfaf455478e39636b9e5619&units=metric`
     );
     const data = await Post.json();
-    // console.log(data.main.temp);
     res.render("weatherData.ejs", { data });
   };
-  // res.redirect("/");
+
   weather();
+});
+app.get("*", (req, res) => {
+  res.render("err.ejs");
 });
 app.listen(process.env.PORT || 3000, () => {
   console.log("SERVER IS ON");
